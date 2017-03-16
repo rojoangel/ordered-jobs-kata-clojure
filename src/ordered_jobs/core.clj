124aa86072b832b7b9991611a10bfb71cd87c5b7
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-16 23:00:30 +0100

    multiple jobs

--- src/ordered_jobs/core.clj
+++ src/ordered_jobs/core.clj
@@ -1,6 +1,19 @@
-(ns ordered-jobs.core)
+(ns ordered-jobs.core
+  (:require [clojure.string :as str]))
+
+(defrecord Dependency [from to])
+
+(defn line->dependency [line]
+  (let [[from to] (str/split line #"=>")]
+    (if to
+      (->Dependency (str/trim from) (str/trim to))
+      (->Dependency (str/trim from) to))))
+
+(defn- parse-jobs [jobs]
+  (let [lines (str/split-lines jobs)]
+    (map line->dependency lines)))
 
 (defn sequence-jobs [jobs]
   (if (empty? jobs)
-    ""
-    "a"))
+    jobs
+    (str/join (map :from (parse-jobs jobs)))))
--- test/ordered_jobs/core_test.clj
+++ test/ordered_jobs/core_test.clj
@@ -6,8 +6,12 @@
 
 (def single-job "a =>")
 
+(def multiple-jobs "a =>\nb =>\nc =>")
+
 (facts "about ordered jobs"
        (fact "if no jobs are given the output is an empty sequence of jobs"
              (sequence-jobs no-job) => "")
        (fact "if a single job is given the output is a single job"
-             (sequence-jobs single-job) => "a"))
+             (sequence-jobs single-job) => "a")
+       (fact "if multiple jobs are given the output sequence contains all jobs in no particular order"
+             (sequence-jobs multiple-jobs) => "abc"))
