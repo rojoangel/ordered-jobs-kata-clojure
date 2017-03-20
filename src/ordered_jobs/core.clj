a1b9d0bf77798eac81c5130b4921372f5bf6e288
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-20 22:22:40 +0100

    if a single dependency is given it positions the dependency before the dependent

--- src/ordered_jobs/core.clj
+++ src/ordered_jobs/core.clj
@@ -13,7 +13,32 @@
   (let [lines (str/split-lines jobs)]
     (map line->dependency lines)))
 
+(defn insert [dependency dependencies-tree]
+  (if (contains? dependencies-tree (:to dependency))
+    (assoc (dissoc dependencies-tree (:to dependency)) (:from dependency) (assoc {} (:to dependency) (get dependencies-tree (:to dependency))))
+    (if-let [[key val] (some #(and (= (:from dependency) (val %)) %) dependencies-tree)]
+      (assoc dependencies-tree key (assoc {} (:from dependency) (:to dependency)))
+      (assoc dependencies-tree (:from dependency) (:to dependency)))))
+
+(defn- link-dependencies [dependencies]
+  (loop [dependencies dependencies
+         dependencies-tree {}]
+    (if (empty? dependencies)
+      dependencies-tree
+      (let [[current & rest] dependencies]
+        (recur rest (insert current dependencies-tree))))))
+
+(defn walk-dependencies [dependencies-tree]
+  (loop [dependencies-tree-entries (seq dependencies-tree)
+         result ""]
+    (if (empty? dependencies-tree-entries)
+      result
+      (let [[current & rest] dependencies-tree-entries]
+        (if (empty? (val current))
+          (recur rest (concat result (key current)))
+          (recur rest (concat result (walk-dependencies (val current)) (key current))))))))
+
 (defn sequence-jobs [jobs]
   (if (empty? jobs)
     jobs
-    (str/join (map :from (parse-jobs jobs)))))
+    (str/join (walk-dependencies (link-dependencies (parse-jobs jobs))))))
--- test/ordered_jobs/core_test.clj
+++ test/ordered_jobs/core_test.clj
@@ -8,10 +8,14 @@
 
 (def multiple-jobs "a =>\nb =>\nc =>")
 
+(def multiple-jobs-single-dependency "a =>\nb =>\nc => b")
+
 (facts "about ordered jobs"
        (fact "if no jobs are given the output is an empty sequence of jobs"
              (sequence-jobs no-job) => "")
        (fact "if a single job is given the output is a single job"
              (sequence-jobs single-job) => "a")
        (fact "if multiple jobs are given the output sequence contains all jobs in no particular order"
-             (sequence-jobs multiple-jobs) => "abc"))
+             (sequence-jobs multiple-jobs) => "abc")
+       (fact "if a single dependency is given it positions the dependency before the dependent"
+             (sequence-jobs multiple-jobs-single-dependency) => "abc"))
